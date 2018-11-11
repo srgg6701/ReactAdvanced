8bbdbea99ca65697b62d3f026213194156ec5e89
Klevtsov <sergei.klevtsov@accenture.com>
2018-11-11 13:48:43 +0200

    Rearranging project

--- src/app-components/index.js
+++ src/app-components/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Navigation from './Navigation';
+import Navigation from '../components/Navigation/';
 
 const LINKS = [
   { label: 'Website', to: 'https://www.robinwieruch.de/' },
