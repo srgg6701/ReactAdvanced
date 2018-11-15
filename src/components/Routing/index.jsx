a01034bda614162b1fa9b1904bf32583dbd098c3
Klevtsov <sergei.klevtsov@accenture.com>
2018-11-15 14:41:36 +0200

    Making nested routing: approach #1

--- src/components/Routing/index.jsx
+++ src/components/Routing/index.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import Fieldset from '../Fieldset'
 
 export default class Routing extends Component {
   render() {
     return (
-      <div>The road to the nothingness starts here!</div>
+      <React.Fragment>
+        <div>The road to the nothingness starts here!</div>
+        <Route path='/routing/default' render={props => (
+          <Fieldset {...props}>
+            <p>Yo, man! We have smt intereting here by <b>default</b>!</p>
+          </Fieldset>
+        )} />
+        <Route path='/routing/:username' render={() => (
+          <p>And here we have some fictional username. Ostensibly...</p>
+        )} />
+      </React.Fragment>
     )
   }
 }
