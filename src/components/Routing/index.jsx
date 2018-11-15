010376bd3c9dbb071c4eba7e5734c73cbe056789
Klevtsov <sergei.klevtsov@accenture.com>
2018-11-15 14:59:48 +0200

    Modifying a nested routing: making submenu within the page

--- src/components/Routing/index.jsx
+++ src/components/Routing/index.jsx
@@ -1,18 +1,34 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 import Fieldset from '../Fieldset'
 
+const base = '/routing';
+
 export default class Routing extends Component {
+  links = {
+    default: `${base}/default`,
+    username: `${base}/:username`,
+  }
   render() {
+    const exact = true;
     return (
       <React.Fragment>
-        <div>The road to the nothingness starts here!</div>
-        <Route path='/routing/default' render={props => (
-          <Fieldset {...props}>
+        <Route path={this.links.base} exact render={() => (
+          <React.Fragment>
+            <div>The road to the nothingness starts here!</div>
+            <ul>
+              <li><NavLink exact={exact} to={base}>Top</NavLink></li>
+              <li><NavLink exact={exact} to={this.links.default}>Default</NavLink></li>
+              <li><NavLink exact={exact} to={this.links.username}>Username</NavLink></li>
+            </ul>
+          </React.Fragment>
+        )} />
+        <Route path={this.links.default} render={props => (
+          <Fieldset legend="Nested routing: default" {...props}>
             <p>Yo, man! We have smt intereting here by <b>default</b>!</p>
           </Fieldset>
         )} />
-        <Route path='/routing/:username' render={() => (
+        <Route path={this.links.username} render={() => (
           <p>And here we have some fictional username. Ostensibly...</p>
         )} />
       </React.Fragment>
